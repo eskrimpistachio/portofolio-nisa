b06df178ae7c7a7d8ef82f704ffbd45f8169485d
fatih <alfatihrizqialfian@gmail.com>
2024-06-02 10:00:29 +0700

    fix footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,10 @@ import { SiGmail } from 'react-icons/si';
 export default function Footer() {
   return (
     <>
-      <div id="contact" className="flex flex-col md:flex-row justify-between satoshi pt-32 pb-8 gap-16 md:gap-0">
+      <div
+        id="contact"
+        className="flex flex-col md:flex-row justify-between satoshi pt-32 pb-8 gap-16 md:gap-0"
+      >
         <div className="flex text-center md:text-left flex-col gap-8">
           <div className="text-4xl">
             <h1 className="font-bold">CONTACT</h1>
@@ -15,14 +18,21 @@ export default function Footer() {
             © Design by Fatih. All rights reserved
           </p>
         </div>
-        <div className="flex justify-center md:justify-normal flex-col gap-4">
-          <div className="flex flex-col gap-8 text-xl md:text-4xl">
-            <FaInstagram/>
-            <a className='text-lg md:text-2xl' href="https://www.instagram.com/choirulnissaa/">@choirulnissa</a>
+        <div className="flex text-center md:text-normal flex-col gap-4">
+          <div className="flex flex-row gap-8 text-4xl">
+            <FaInstagram />
+            <a
+              className="text-2xl"
+              href="https://www.instagram.com/choirulnissaa/"
+            >
+              @choirulnissa
+            </a>
           </div>
-          <div className="flex flex-col md:flex-row gap-8 text-xl md:text-4xl">
+          <div className="flex flex-row md:flex-row gap-8 text-4xl">
             <SiGmail />
-            <a className='text-xl md:text-2xl' href="">nhartanto26@gmail.com</a>
+            <a className="text-2xl" href="mailto:nhartanto26@gmail.com">
+              nhartanto26@gmail.com
+            </a>
           </div>
         </div>
       </div>
